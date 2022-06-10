efad137d8e80cd59a744f40f81b7129cc208ed9d
Drivahah <s212975@student.dtu.com>
2022-06-10 13:20:58 +0200

    Load phenotype

--- Scripts/01_Load.R
+++ Scripts/01_Load.R
@@ -1,1 +1,8 @@
+library("DESeq2")
+library("ggplot2")
+library("tidyverse")
+library("dplyr")
+library("readr")
+
 raw <- read.table(file = gzfile('Data/_raw/TCGA-SKCM.htseq_counts.tsv.gz','rt'), sep = '\t', header = TRUE)
+phenotype <- read_tsv(file = gzfile('Data/_raw/TCGA-SKCM.GDC_phenotype.tsv.gz'), col_names=TRUE)
