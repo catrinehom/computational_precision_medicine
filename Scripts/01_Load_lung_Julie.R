cbdb6e551ad2fd11c47537d234c1a026c67a7c96
julzim <julie-zimmer@hotmail.com>
2022-06-14 17:14:48 +0200

    add knn and DTC

--- Scripts/01_Load_lung_Julie.R
+++ Scripts/01_Load_lung_Julie.R
@@ -4,8 +4,8 @@ library(GSVA)
 library(ComplexHeatmap)
 
 ## Load data
-raw <- read.delim("Data/_raw/HiSeqV2-2", row.names=1)
-lung_data <- round((2^raw)-1, digits = 0)
+lung_data <- read.delim("Data/_raw/HiSeqV2-2", row.names=1)
+#lung_data <- round((2^raw)-1, digits = 0)
 colnames(lung_data) <- gsub(pattern = "\\.", replacement = "-", x = colnames(lung_data))
 colnames(raw) <- gsub(pattern = "\\.", replacement = "-", x = colnames(raw))
 
@@ -99,31 +99,73 @@ pred_vector <- c()
 # loop over samples (columns)
 for(i in 1:ncol(lung_data_sub)) {
   # make a training matrix consisting of all samples but the one you leave out
-  training <- CIT_sub[,-i]
+  training <- lung_data_sub[,-i]
   # remove that samples class from the class vector
-  training_classes <- CIT_classes[-i]
+  training_classes <- lung_pheno$Expression_Subtype[-i]
   # make a test vector consisting of only that sample
-  test <- CIT_sub[,i]
+  test <- lung_data_sub[,i]
   # get the class of that sample
-  test_class <- CIT_classes[i]
+  test_class <- lung_pheno$Expression_Subtype[i]
   
   # make an empty centroid matrix
   centroids <- NULL
-  # loop over each of the six classes
-  for (class in unique(CIT_classes)) {
+  # loop over each of the three classes
+  for (class in unique(lung_pheno$Expression_Subtype)) {
     # for each of these classes, subset the training matrix to samples belonging to that class, and calculate the mean expression of each gene in the class
     class_centroid <- rowMeans(training[,training_classes==class])
     # add the mean vector to the centroids matrix
     centroids <- cbind(centroids, class_centroid)
   }
   # add colnames to the centroid matrix
-  colnames(centroids) <- unique(CIT_classes)
+  colnames(centroids) <- unique(lung_pheno$Expression_Subtype)
   # calculate the distance of the test sample to the centroids
   d <- as.matrix(dist(t(cbind(centroids, test))))
   # assign the class of the closest centroid
-  class_pred <- names(which.min(d[1:6,7]))
+  class_pred <- names(which.min(d[1:3,4]))
   # check if you got it right and make a logical vector
   pred_vector <- c(pred_vector, test_class==class_pred)
 }
 # check how many of the cases you got it right
 table(pred_vector)
+
+## kNN
+## Do a leave-one-out classification of CIT training samples using kNN
+if(!require('class')) {
+  install.packages('class')
+  library('class')
+}
+
+
+
+pred <- integer(ncol(lung_data_sub)-1)
+
+for (k in 1:ncol(lung_data_sub)-1) {
+  answers = integer(ncol(lung_data_sub))
+  for(i in 1:ncol(lung_data_sub)){
+    test_knn <- subset(lung_data_sub, select = c(i)) 
+    train_knn <- subset(lung_data_sub, select = -c(i)) 
+    
+    cl <- subset(t(lung_pheno$Expression_Subtype), select = -c(i)) 
+    cl <- factor(t(cl))
+    train_knn <- t(train_knn)
+    test_knn <- t(test_knn)
+    
+    subtype_knn <- as.character(knn(train_knn, test_knn, cl, k = k, prob=TRUE))
+    
+    answers[i] = subtype_knn == t(lung_pheno$Expression_Subtype[i])
+    
+  }
+
+pred[k] <- sum(answers/ncol(lung_data_sub))
+
+}
+
+pred_k <-cbind(seq(1,275),pred)
+colnames(pred_k) <- c("k","pred")
+
+df <- data.frame(pred_k)
+ggplot(df, aes(x= k, y = pred)) +
+  geom_point()
+
+#k=10
+table(answers)
